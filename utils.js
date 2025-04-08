c4d378d3dac3ed80a4581e33ef5d2606ad06acbd
gab.ry / Gabriele + Potenza <gabriele.potenza9907@gmail.com>
2025-04-08 16:29:10 +0200

    prova

---
+++ utils.js
@@ -0,0 +1,32 @@
+import PromptSync from 'prompt-sync';
+
+const prompt = PromptSync();
+
+export function memorizzaUtenti(numero_alunni){
+    let oggetto;
+    
+    let utenti = Array.of(numero_alunni);
+
+    for(let i = 0; i < numero_alunni; i++){
+        oggetto = {
+            "Nome" : prompt("Nome alunno: "),
+            "Eta"  : Number.parseInt(prompt("Età alunno: ")),
+        }
+        utenti[i] = oggetto;
+    } 
+    return utenti;   
+}
+
+export const contaAlunni = (registro) =>{
+    let cont = 0;
+    registro.forEach(oggetto => {
+        if(oggetto.Eta >= 18) cont++;
+    });
+    return cont;
+}
+
+export const stampaAlunni = function(registro){
+    registro.forEach(oggetto => {
+        if(oggetto.Eta >= 18) console.log(oggetto.Nome);
+    });
+}
