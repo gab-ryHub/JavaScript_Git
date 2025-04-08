97ed4d566966b48a387d8725bce13ba3c56eabf2
gab.ry / Gabriele + Potenza <gabriele.potenza9907@gmail.com>
2025-04-08 16:43:31 +0200

    versione senza menù

--- index.js
+++ index.js
@@ -2,29 +2,15 @@ import PromptSync from 'prompt-sync';
 const prompt = PromptSync();
 import * as u from "./utils.js";
 
-let scelta = 0;
 let registro;
+let numero_alunni = Number.parseInt(prompt("Numero alunni: "));
 
-do{
-    console.log("\nMenù.\n 1-Memorizza alunni.\n 2-Conta alunni.\n 3-Stampa alunni maggiorenni.\n 4-Uscita");
-    scelta = Number.parseInt(prompt("Fai una scelta: "));
+console.log("\n")
 
-    switch(scelta){
-        case 1:
-            let numero_alunni = Number.parseInt(prompt("Numero alunni: "));
-            console.log("\n")
-            registro = u.memorizzaUtenti(numero_alunni);
-            console.log(registro)
-            break;
-        case 2:
-            console.log("\nNumero alunni maggiorenni: " + u.contaAlunni(registro));
-            break;
-        case 3:
-            console.log("\nNome alunni maggiorenni: ")
-            u.stampaAlunni(registro);
-            break;
-        case 4:
-            console.log("\nUscita");
-            break;
-    }
-}while(scelta != 4);
+registro = u.memorizzaUtenti(numero_alunni);
+console.log(registro)
+
+console.log("\nNumero alunni maggiorenni: " + u.contaAlunni(registro));
+
+console.log("\nNome alunni maggiorenni: ")
+u.stampaAlunni(registro);
